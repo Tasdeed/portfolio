58de79de639b9546cd0fc29d144ab8d496d23dcf
Tasdeed <thossai4@binghamton.edu>
2022-12-23 16:35:52 -0500

    working on mobile sidebar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { FcAutomatic } from "react-icons/fc";
 
 const Navbar = () => {
   return (
@@ -34,6 +36,45 @@ const Navbar = () => {
               </li>
             </Link>
           </ul>
+          <div className="md:hidden">
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+      <div className="fixed left-0 top-0 w-full h-screen bg-black/60">
+        <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-white p-10 ease-in duration-500">
+          <div>
+            <div className="flex w-full items-center justify-between">
+              <FcAutomatic size={100} />
+              <div className="rounded-full shadow-md shadow-gray-400 p-3 cursor-pointer">
+                <AiOutlineClose />
+              </div>
+            </div>
+          </div>
+          <div className="py-4 flex flex-col">
+            <ul className="uppercase">
+              <Link href="/">
+                <li className="pb-4 pt-6 text-sm border-t border-gray-300">
+                  Home
+                </li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">About</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Skills</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Projects</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Contact</li>
+              </Link>
+            </ul>
+            <div className="pt-40">
+              <p className="uppercase tracking-widest">Hit me up!</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
