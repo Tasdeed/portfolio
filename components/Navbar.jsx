ab9aa65b11eff331f4729031b30f7e6bfa7623d9
Tasdeed <thossai4@binghamton.edu>
2022-12-24 21:30:45 -0500

    finished mobile sidebar functionality with placeholder social logos

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,8 +2,21 @@ import Image from "next/image";
 import Link from "next/link";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { FcAutomatic } from "react-icons/fc";
+import {
+  BsLinkedin,
+  BsGithub,
+  BsMailbox,
+  BsFillPersonLinesFill,
+} from "react-icons/bs";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [side, setSide] = useState(false);
+
+  const handleSide = () => {
+    setSide(!side);
+  };
+
   return (
     <div className="fixed w-full h-20 z-[100]">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
@@ -36,17 +49,30 @@ const Navbar = () => {
               </li>
             </Link>
           </ul>
-          <div className="md:hidden">
+          <div onClick={handleSide} className="md:hidden cursor-pointer">
             <AiOutlineMenu size={25} />
           </div>
         </div>
       </div>
-      <div className="fixed left-0 top-0 w-full h-screen bg-black/60">
-        <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-white p-10 ease-in duration-500">
+      <div
+        className={
+          side ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/60" : ""
+        }
+      >
+        <div
+          className={
+            side
+              ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-white p-10 ease-in duration-500"
+              : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+          }
+        >
           <div>
             <div className="flex w-full items-center justify-between">
               <FcAutomatic size={100} />
-              <div className="rounded-full shadow-md shadow-gray-400 p-3 cursor-pointer">
+              <div
+                onClick={handleSide}
+                className="rounded-full shadow-md shadow-gray-400 p-3 cursor-pointer"
+              >
                 <AiOutlineClose />
               </div>
             </div>
@@ -72,7 +98,23 @@ const Navbar = () => {
               </Link>
             </ul>
             <div className="pt-40">
-              <p className="uppercase tracking-widest">Hit me up!</p>
+              <p className="uppercase tracking-widest text-purple-500">
+                Hit me up!
+              </p>
+              <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                <div className="rounded-full shadow-md shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <BsLinkedin />
+                </div>
+                <div className="rounded-full shadow-md shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <BsGithub />
+                </div>
+                <div className="rounded-full shadow-md shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <BsMailbox />
+                </div>
+                <div className="rounded-full shadow-md shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <BsFillPersonLinesFill />
+                </div>
+              </div>
             </div>
           </div>
         </div>
