376be93cd1b6f0d2e8c1538996ceddd4aabf8094
Tasdeed <thossai4@binghamton.edu>
2022-12-29 16:06:12 -0500

    finished contact section

--- components/Contact.js
+++ components/Contact.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 import {
   BsFillPersonLinesFill,
   BsGithub,
   BsLinkedin,
   BsMailbox,
 } from "react-icons/bs";
+import { HiChevronDoubleUp } from "react-icons/hi";
 import ContactImg from "../public/thom-milkovic.jpg";
 
 const Contact = () => {
@@ -16,7 +18,7 @@ const Contact = () => {
         </p>
         <h2 className="py-4">Get in Touch</h2>
         <div className="grid lg:grid-cols-5 gap-8">
-          <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-300 rounded-xl p-4">
+          <div className="col-span-3 lg:col-span-2 w-full h-full shadow-lg shadow-gray-300 rounded-xl p-4">
             <div className="lg:p-4 h-full">
               <div>
                 <Image
@@ -34,7 +36,7 @@ const Contact = () => {
               </div>
               <div>
                 <p className="uppercase pt-8">Connect With Me</p>
-                <div className="flex items-center justify-between py-4">
+                <div className="flex items-center justify-evenly py-4">
                   <div className="rounded-full shadow-md shadow-gray-300 p-6 cursor-pointer hover:scale-125 ease-in duration-300">
                     <BsLinkedin />
                   </div>
@@ -51,6 +53,61 @@ const Contact = () => {
               </div>
             </div>
           </div>
+          <div className="col-span-3 w-full h-auto shadow-lg shadow-gray-300 rounded-xl lg:p-4">
+            <div className="p-4">
+              <form>
+                <div className="grid md:grid-cols-2 gap-4 w-full py-2">
+                  <div className="flex flex-col">
+                    <label className="uppercase text-sm py-2">Name</label>
+                    <input
+                      className="border-2 rounded-lg p-3 flex border-gray-300"
+                      type="text"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label className="uppercase text-sm py-2">
+                      Phone Number
+                    </label>
+                    <input
+                      className="border-2 rounded-lg p-3 flex border-gray-300"
+                      type="text"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className="uppercase text-sm py-2">Email</label>
+                  <input
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                    type="email"
+                  />
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className="uppercase text-sm py-2">Subject</label>
+                  <input
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                    type="text"
+                  />
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className="uppercase text-sm py-2">Message</label>
+                  <textarea
+                    className="border-2 rounded-lg p-3 border-gray-300"
+                    rows="16"
+                  ></textarea>
+                </div>
+                <button className="w-full p-4 text-gray-100 mt-4 shadow-none">
+                  Send Message
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center py-12">
+          <Link href="/">
+            <div className="animate-bounce rounded-full shadow-md shadow-gray-300 p-4 cursor-pointer hover:scale-125 ease-in duration-300">
+              <HiChevronDoubleUp className="text-amber-500" size={35} />
+            </div>
+          </Link>
         </div>
       </div>
     </div>
